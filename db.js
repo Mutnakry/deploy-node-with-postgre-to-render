9aff1ab53883bb5ed935b4f999b7220b8d22191b
Nakry Mut <nakrymut375@gmail.com>
2025-01-04 11:53:46 +0700

    Your commit message

--- db.js
+++ db.js
@@ -1,14 +1,25 @@
+// const { Pool } = require('pg');
+// require('dotenv').config();
+
+// // Create a pool using environment variables
+// const pool = new Pool({
+//   host: 'localhost',
+//   host: process.env.DB_HOST,
+//   port: process.env.DB_PORT,
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME
+// });
+
+// module.exports = pool;
+
+
 const { Pool } = require('pg');
 require('dotenv').config();
 
-// Create a pool using environment variables
 const pool = new Pool({
-  host: 'localhost',
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME
+    connectionString: process.env.DATABASE_URL, // Ensure DATABASE_URL is set in your .env
+    ssl: { rejectUnauthorized: false }, // Necessary for hosted PostgreSQL (e.g., Render or Heroku)
 });
 
 module.exports = pool;
